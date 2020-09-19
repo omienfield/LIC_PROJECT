6ffc901644776f9fe59cf765cd26f8c610b06fab
omkar meghanath patil <omkar.patil2@morningstar.com>
2020-09-19 14:38:31 +0530

    SMALL COMMIT FOR MAN HUGE COMMIT FOR MANKIND

---
+++ LIC/src/test/java/com/lic/starter/LicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lic.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
